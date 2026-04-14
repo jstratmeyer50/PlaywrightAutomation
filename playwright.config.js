e595cd18990fcb4dfddf063043392e6313a48ac5
jstratmeyer50 <jessstratmeyer@gmail.com>
2026-04-14 17:56:33 -0400

    config change

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [['List'], ['html', {open: 'never'}]] : [['html']],
+  reporter: process.env.CI ? [['list'], ['html', {open: 'never'}]] : [['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
