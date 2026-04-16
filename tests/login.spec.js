84205ccc2496255e8e7bb94cd9ed0127b2ee3f19
jstratmeyer50 <jessstratmeyer@gmail.com>
2026-04-16 18:04:52 -0400

    login spec and checkout spec finished

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -15,5 +15,19 @@ test.describe("Login", () => {
     await page.getByPlaceholder("Password").type("secret_sauce");
     await page.getByRole("button", { name: "Login" }).click();
 
+    // Verify successful login
+    await expect(page).toHaveURL(/.*inventory.html/);
+    
+    await page.locator('[data-test="item-4-title-link"]').click();
+    await expect(page.locator('.inventory_details_name')).toHaveText("Sauce Labs Backpack");
+    await page.getByRole("button", { name: "Add to cart" }).click();
+    await page.getByRole("button", { name: "Remove" }).click();
+    await page.getByRole("button", { name: "Back to products" }).click();
+    await page.getByRole("button", { name: "Open Menu" }).click();
+    await page.getByRole("link", { name: "Logout" }).click();   
+    
+    // Verify successful logout
+    await expect(page).toHaveURL("https://www.saucedemo.com/"); 
+
   });
 });
